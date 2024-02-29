d118eec51a72bf32154741e06082a57337b1c4e0
Andrew (PyroAVR) Meyer <ahm7197@rit.edu>
2024-02-28 21:39:39 -0800

    posix stream init

--- encoder/include/twig/streams/posix.h
+++ encoder/include/twig/streams/posix.h
@@ -2,6 +2,7 @@
 #include <twig/stream.h>
 
 #include <twig/streams/byte_queue.h>
+#include <twig/packer.h>
 
 #include <stddef.h>
 #include <stdio.h>
@@ -16,6 +17,11 @@ typedef struct {
         POSIX_STREAM_FILE,
     } tag;
 } posix_stream_t;
+
+static inline packer_ctx_t *posix_stream_init(packer_ctx_t *ctx, posix_stream_t *stream) {
+    ctx->stream = stream;
+    return ctx;
+}
 
 posix_stream_t *posix_stream_from_file(posix_stream_t *stream, FILE *f);
 posix_stream_t *posix_stream_from_queue(posix_stream_t *stream, queue_t *q);
